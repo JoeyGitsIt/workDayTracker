aae382f285f2de8c6b126aca5fc5ad570df3cf2e
JoeyGitsIt <josephperri3@gmail.com>
2021-09-24 21:06:56 -0400

    added html buttons and boxes

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// use moment.js to show current time/date info for top of page
+// for workday hours (9-5) place bootstrap objects/containers going down the page
+// box needs to change color depending on time (grey for past, red preset, green future)
+// each timeblock has a text box where I can enter information that needs to be done in that hour
+// There is a save button to the right of those things, when button is clicked then it saves the text/time to local storage
